3b5cfc905f501731925ad558b95a25e9f6847a39
Coge92 <mh.melting@gmail.com>
2024-01-16 01:33:52 +0100

    Dot selector à terminer

--- assets/script.js
+++ assets/script.js
@@ -17,23 +17,35 @@ const slides = [
 	}
 ]
 
-console.log(slides.length);
+console.log(slides.length); // compte le nombre d'images
 
-let arrowLeft = document.querySelector(".arrow_left")
-let arrowRight = document.querySelector(".arrow_right")
+let arrowLeft = document.querySelector(".arrow_left") // objet arrow left
+let arrowRight = document.querySelector(".arrow_right") // objet arrow right
+let index = 0 
 
 console.log(arrowLeft);
 console.log(arrowRight);
 
-arrowLeft.addEventListener("click", () => {
+// ***** Event Listener sur arrow left
+arrowLeft.addEventListener("click", () => {  
 
-	alert("T'as cliqué à gauche")
+	console.log("T'as cliqué à gauche")
+	index--
+	console.log(index);
+	sliderMoteur(index)
 })
+
+// ***** Event Listener sur arrow right
 
 arrowRight.addEventListener("click", () => {
 
-	alert("T'as cliqué à droite")
+	console.log("T'as cliqué à droite")
+	index++
+	console.log(index);
+	sliderMoteur(index)
 })
+
+// ajout des bullets points 
 
 let divContainerDot = document.querySelector(".dots")
 
@@ -43,11 +55,35 @@ for (let i = 0; i < slides.length; i++) {
 
 	let pointsSlider = document.createElement("div")
 	pointsSlider.classList.add("dot")
+	pointsSlider.setAttribute("id", i)
 	divContainerDot.appendChild(pointsSlider)
-
 }
 
 
+let bannerImg = document.querySelector(".banner-img")
+
+console.log(bannerImg);
+
+console.log(index);
+
+function sliderMoteur(x) {
+
+	let sourceImage = slides[x].image
+	console.log(sourceImage);
+	bannerImg.setAttribute("src","./assets/images/slideshow/" + sourceImage)
+
+	let containerTextImage = document.querySelector("#banner p")
+	console.log(containerTextImage);
+
+	let textImage = slides[x].tagLine
+	containerTextImage.innerHTML = textImage
+
+	let dotSelected = document.getElementById(x)
+	console.log(dotSelected);
+	dotSelected.classList.toggle("dot_selected")
+
+
+}
 
 
 
